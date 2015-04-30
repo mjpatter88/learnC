acbd46019dd456d32ac0ae695778ee14b4173793
Michael Patterson <mjpatter88@gmail.com>
2015-04-30 02:16:22 -0400

    Added workfiles for chapters 11, 12, 13, and 14.

---
+++ ex12/ex12.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    int i=0;
+
+    if(argc==1)
+    {
+        printf("You didn't enter any arguments.\n");
+    }
+    else if(argc > 1 && argc < 4)
+    {
+        printf("Here are your arguments:\n");
+        for(i=0; i<argc; i++)
+        {
+            printf("Arg %d: %s\n", i, argv[i]);
+        }
+    }
+    else
+    {
+        printf("You have too many arguments.\n");
+    }
+
+    return 0;
+}
