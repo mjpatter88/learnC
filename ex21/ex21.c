4fe0b5a41f3fedb845226b8d645ffa7944a08210
Michael Patterson <mjpatter88@gmail.com>
2015-06-21 15:05:00 -0400

    Added work files for chapter 21.

---
+++ ex21/ex21.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(int argc, char* argv[])
+{
+    printf("Size of int: %lu\n", sizeof(int));
+    printf("Size of double: %lu\n", sizeof(double));
+    printf("Size of float: %lu\n", sizeof(float));
+    printf("Size of char: %lu\n", sizeof(char));
+    printf("Size of void: %lu\n", sizeof(void));
+    printf("Size of long: %lu\n", sizeof(long));
+    printf("Hello");
+    return 0;
+}
